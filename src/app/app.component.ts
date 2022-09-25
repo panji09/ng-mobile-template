89ec21d8ab9c15bccd69d32532feaad6fb490313
panji musthofa <panji09@gmail.com>
2022-09-25 22:06:43 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,54 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-13-ng-zorro-mobile-template';
+  hidden: boolean = false;
+  fullScreen: boolean = false;
+  topFlag: boolean = false;
+  tintColor: string = '#108ee9';
+  unselectedTintColor: string = '#888';
+  tabbarStyle: object = { height: '400px' };
+  selectedIndex: number = 1;
+
+  showTabBar(event: any) {
+    event.preventDefault();
+    this.hidden = !this.hidden;
+  }
+
+  showNextTabBar(event: any) {
+    event.preventDefault();
+    const PANE_COUNT = 4;
+    if (this.selectedIndex == PANE_COUNT - 1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
+    console.log('selectedIndex: ', this.selectedIndex);
+  }
+
+  showFullScreen(event: any) {
+    event.preventDefault();
+    this.fullScreen = !this.fullScreen;
+    this.tabbarStyle = this.fullScreen
+      ? {
+          position: 'fixed',
+          height: '100%',
+          width: '100%',
+          top: 0,
+        }
+      : { height: '400px' };
+  }
+
+  changePosition(event: any) {
+    event.preventDefault();
+    this.topFlag = !this.topFlag;
+  }
+
+  tabBarTabOnPress(pressParam: any) {
+    console.log('onPress Params: ', pressParam);
+    this.selectedIndex = pressParam.index;
+  }
 }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, NgZorroAntdMobileModule],
+  exports: [NgZorroAntdMobileModule],
+})
+export class SharedModule {}
